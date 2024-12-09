1277b77bb2ca0e2c21af2d9e3d9c2e06fe960a7e
Mahesh Shantaram <ms@thecontrarian.in>
2024-12-09 17:49:54 +0530

    next change

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,25 @@ import streamlit as st
 import pandas as pd
 
 st.title("🤖 Machine Learning App")
-
 st.write("Welcome to world of Machine Learning with Streamlit.")
 
-df = pd.read_csv("penguins_cleaned.csv")
-df
+with st.expander("Data"):
+    st.write("**Raw Data**")
+    df = pd.read_csv("penguins_cleaned.csv")
+    df
+
+    st.write("**X**")
+    X = df.drop("species",axis=1)
+    X
+
+    st.write("**y**")
+    y = df.species
+    y
+
+with st.expander("Data Visualization"):
+    st.scatter_chart(
+        df,
+        x="bill_length_mm",
+        y="body_mass_g",
+        color="species",
+    )
