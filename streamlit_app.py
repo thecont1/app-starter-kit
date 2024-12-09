d858c21d3ae4fd5abadbd0e131cfee52e95f6739
Mahesh Shantaram <ms@thecontrarian.in>
2024-12-09 17:25:42 +0530

    next change

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,4 @@ import streamlit as st
 st.title("🤖 Machine Learning App")
 
 st.write("Welcome to world of Machine Learning with Streamlit.")
+st.write("If what is said is true, you should see this line updated automatically!")
