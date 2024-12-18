bd256457abecfcdbf51d71902271cd27b0810327
Mahesh Shantaram <ms@thecontrarian.in>
2024-12-18 23:56:54 +0530

    pre-vacation hail mary batch commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,12 @@
 import streamlit as st
 import pandas as pd
 
-st.title("🤖 Machine Learning App")
-st.write("Welcome to world of Machine Learning with Streamlit.")
+st.title("🤖 Namma Metro Ridership Tracker")
+st.write("The Bangalore Metro Rail Corporation Limited (BMRCL) publishes daily ridership data every 24 hours. Unfortunately, they do not provide historical data beyond one day. This repository contains a Python script and Jupyter Notebook to automate the process of downloading ridership data from BMRCL and storing it in a csv file. As the dataset evolves over time, it will allow for analysis of ridership and usage patterns.")
 
 with st.expander("Data"):
     st.write("**Raw Data**")
-    df = pd.read_csv("penguins_cleaned.csv")
-    df
+    df = pd.read_csv("https://raw.githubusercontent.com/thecont1/namma-metro-ridership-tracker/refs/heads/main/NammaMetro_Ridership_Dataset.csv")
 
     st.write("**X**")
     X = df.drop("species",axis=1)
